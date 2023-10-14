13d2a6832ecf636a417ed82ea2218424f8f6724a
Alex Koval <al3xander.koval@gmail.com>
2023-10-14 21:53:03 +0100

    simplify stuff

--- mnswpr.c
+++ mnswpr.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define EXIT_SUCCESS 0
+#define EXIT_ERROR 1
 
-#define FIELD_WIDTH 20
-#define FIELD_HEIGHT 20
-#define MINES_COUNT 30
+#define FIELD_WIDTH 10
+#define FIELD_HEIGHT 10
+#define MINES_COUNT 10
 
 typedef enum { OPEN, CLOSE, MARK_AS_MINE } CellStatus;
 typedef struct {
-  char value;
+  int value;
   CellStatus status;
 } Cell;
 
@@ -18,29 +20,29 @@ typedef struct {
   Cell cels[FIELD_WIDTH][FIELD_HEIGHT];
 } Field;
 
-typedef enum { FINISHED, ACTIVE } GameStatus;
+typedef enum { PENDING, ACTIVE, FINISHED } GameStatus;
 
 typedef struct {
-  Field* field;
+  Field *field;
   GameStatus status;
   int mines_placed;
 } Game;
 
-Field* createField() {
+Field *createField() {
   Field *f = malloc(sizeof(Field));
   srand(time(0));
 
-  for(int i=0; i<FIELD_HEIGHT; ++i) {
-    for(int j=0; j<FIELD_WIDTH; ++j) {
+  for (int i = 0; i < FIELD_HEIGHT; ++i) {
+    for (int j = 0; j < FIELD_WIDTH; ++j) {
       f->cels[i][j].value = 0;
       f->cels[i][j].status = CLOSE;
     }
   }
 
   int mines_placed = 0;
-  while(mines_placed < MINES_COUNT) {
-    int pos_x = rand()%FIELD_WIDTH;
-    int pos_y = rand()%FIELD_HEIGHT;
+  while (mines_placed < MINES_COUNT) {
+    int pos_x = rand() % FIELD_WIDTH;
+    int pos_y = rand() % FIELD_HEIGHT;
 
     if (f->cels[pos_x][pos_y].value == 0) {
       f->cels[pos_x][pos_y].value = -1;
@@ -51,11 +53,11 @@ Field* createField() {
   return f;
 }
 void printField(Field *f, int is_debug) {
-  printf("INFO: i=rows, j=cols\n\n");
-  printf("  0 1 2 3 4 5 6 7 8 9\n");
-  for(int i=0; i<FIELD_HEIGHT; ++i) {
-    printf("%i ", i);
-    for(int j=0; j<FIELD_WIDTH; ++j) {
+  printf("   0 1 2 3 4 5 6 7 8 9\n");
+  printf("======================\n");
+  for (int i = 0; i < FIELD_HEIGHT; ++i) {
+    printf("%i| ", i);
+    for (int j = 0; j < FIELD_WIDTH; ++j) {
       if (is_debug) {
         if (f->cels[i][j].value == -1) {
           printf("* ");
@@ -74,137 +76,155 @@ void printField(Field *f, int is_debug) {
     }
     printf("\n");
   }
+  printf("======================\n");
 }
 
+int isEndOfTheGame(Game *g) {
+  int result = 0;
 
-void batchOpen(Field* f, int i, int j) {
-  if (i < 0 || i > FIELD_HEIGHT-1 || j < 0 || j > FIELD_WIDTH-1) {
+  for (int i = 0; i < FIELD_HEIGHT; ++i) {
+    for (int j = 0; j < FIELD_WIDTH; ++j) {
+      if (g->field->cels[i][j].status == OPEN ||
+          g->field->cels[i][j].status == MARK_AS_MINE) {
+        result = 1;
+      } else {
+        return 0;
+      }
+    }
+  }
+
+  return result;
+}
+
+int isInvalidCoordinates(int i, int j) {
+  return i < 0 || i > FIELD_HEIGHT - 1 || j < 0 || j > FIELD_WIDTH - 1;
+}
+
+void batchOpen(Field *f, int i, int j) {
+  // check that we are in table bounds
+  if (isInvalidCoordinates(i, j)) {
     return;
   }
-  if ( f->cels[i][j].status == OPEN ) {
+  if (f->cels[i][j].status == OPEN) {
     return;
   }
 
   if (f->cels[i][j].value == 0) {
     f->cels[i][j].status = OPEN;
-    batchOpen(f, i-1, j);
-    batchOpen(f, i, j+1);
-    batchOpen(f, i, j-1);
-    batchOpen(f, i+1, j);
-    batchOpen(f, i+1, j+1);
-    batchOpen(f, i+1, j-1);
-    batchOpen(f, i-1, j-1);
-    batchOpen(f, i-1, j+1);
+    batchOpen(f, i - 1, j);
+    batchOpen(f, i, j + 1);
+    batchOpen(f, i, j - 1);
+    batchOpen(f, i + 1, j);
+    batchOpen(f, i + 1, j + 1);
+    batchOpen(f, i + 1, j - 1);
+    batchOpen(f, i - 1, j - 1);
+    batchOpen(f, i - 1, j + 1);
   } else {
     f->cels[i][j].status = OPEN;
   }
 }
 
-Game start() {
-  Game game;
-  Field* f = createField();
-  game.field = f;
-  game.status = ACTIVE;
-  game.mines_placed = 0;
+void updateCell(Field *f, int i, int j) {
+  if (isInvalidCoordinates(i, j)) {
+    return;
+  }
+  if (f->cels[i][j].value != -1) {
+    f->cels[i][j].value++;
+  }
+}
 
-  return game;
+void updateCellNeighbours(Field *f, int i, int j) {
+  updateCell(f, i - 1, j);
+  updateCell(f, i, j + 1);
+  updateCell(f, i, j - 1);
+  updateCell(f, i + 1, j);
+  updateCell(f, i + 1, j + 1);
+  updateCell(f, i + 1, j - 1);
+  updateCell(f, i - 1, j - 1);
+  updateCell(f, i - 1, j + 1);
 }
 
 void calcWeights(Field *f) {
-  for(int i=0; i<FIELD_HEIGHT; ++i) {
-    for(int j=0; j<FIELD_WIDTH; ++j) {
-      int is_mine = f->cels[i][j].value == -1;
-      if (is_mine) {
-        // north
-        if(i>0) {
-          if (f->cels[i-1][j].value != -1){
-            f->cels[i-1][j].value++;
-          }
-        }
-        // n-e
-        if(i != 0 && j < FIELD_WIDTH-1) {
-          if (f->cels[i-1][j+1].value != -1){
-            f->cels[i-1][j+1].value++;
-          }
-        }
-        // n-w
-        if(i != 0 && j > 0) {
-          if (f->cels[i-1][j-1].value != -1){
-            f->cels[i-1][j-1].value++;
-          }
-        }
-        // south
-        if(i < FIELD_HEIGHT -1) {
-          if (f->cels[i+1][j].value != -1) {
-            f->cels[i+1][j].value++;
-          }
-        }
-        // s-e
-        if(i < FIELD_HEIGHT -1 && j < FIELD_WIDTH-1) {
-          if (f->cels[i+1][j+1].value != -1) {
-            f->cels[i+1][j+1].value++;
-          }
-        }
-        // s-w
-        if(i < FIELD_HEIGHT-1 && j > 0) {
-          if (f->cels[i+1][j-1].value != -1) {
-            f->cels[i+1][j-1].value++;
-          }
-        }
-        // east
-        if(j < FIELD_WIDTH-1) {
-          if (f->cels[i][j+1].value != -1) {
-            f->cels[i][j+1].value++;
-          }
-        }
-        // west
-        if(j>0) {
-          if (f->cels[i][j-1].value != -1) {
-            f->cels[i][j-1].value++;
-          }
-        }
+  for (int i = 0; i < FIELD_HEIGHT; ++i) {
+    for (int j = 0; j < FIELD_WIDTH; ++j) {
+      int isMine = f->cels[i][j].value == -1;
+      if (isMine) {
+        updateCellNeighbours(f, i, j);
       }
     }
   }
 }
 
-int main(int argc, char *argv[])
-{
-  Game game = start();
-  Field* f = game.field;
-  printField(f, 0);
-  printf("\n");
+Game start() {
+  Game game;
+  Field *f = createField();
   calcWeights(f);
-  printField(f, 1);
+  printField(f, 0);
+
+  game.field = f;
+  game.status = PENDING;
+  game.mines_placed = 0;
+
+  return game;
+}
+
+int main(int argc, char *argv[]) {
+  Game game = start();
+  Field *f = game.field;
 
   char *input = NULL;
   size_t len = 0;
 
   while (game.status != FINISHED) {
+    int gameEnded = isEndOfTheGame(&game);
+    if (gameEnded) {
+      printf("You win!\n");
+      return EXIT_SUCCESS;
+    }
+
     printf("Mines left: %i\n", MINES_COUNT - game.mines_placed);
     printf("Enter cell index to open, prefix `f` to put a flag(i,j): ");
     getline(&input, &len, stdin);
     char command = input[0];
+    game.status = ACTIVE;
 
     if (command == 'f') {
-      int i = input[1] - '0'; // it converts char to int index
-      int j = input[3] - '0';
+      input = &input[1];
+      char * ptr = strtok(input, ",");
+      int i = atoi(ptr);
+      int j = atoi(input);
+      printf("%i", isInvalidCoordinates(i, j));
+      if (isInvalidCoordinates(i, j)) {
+        printf("ERROR: Invalid coordinates: i=%i, j=%i\n", i, j);
+        continue;
+      }
       f->cels[i][j].status = MARK_AS_MINE;
       game.mines_placed++;
       printField(f, 0);
+    } else if (command == 'q') {
+      printf("Exiting\n");
+      return EXIT_SUCCESS;
     } else {
-      int i = input[0] - '0'; // it converts char to int index
-      int j = input[2] - '0';
+      char * ptr = strtok(input, ",");
+      int i = atoi(ptr);
+      int j = atoi(input);
+      printf("%i", isInvalidCoordinates(i, j));
+      if (isInvalidCoordinates(i, j)) {
+        printf("ERROR: Invalid coordinates: i=%i, j=%i\n", i, j);
+        continue;
+      }
 
-      if(f->cels[i][j].value == -1) {
+      if (f->cels[i][j].value == -1) {
         game.status = FINISHED;
         printf("GAME OVER\n");
+        printField(f, 1);
       } else if (f->cels[i][j].value == 0) {
-        // implement batch open algorithm
         batchOpen(f, i, j);
-        /*f->cels[i][j].status = OPEN;*/
         printField(f, 0);
       } else {
+        if (f->cels[i][j].status == MARK_AS_MINE) {
+          game.mines_placed--;
+        }
         f->cels[i][j].status = OPEN;
         printField(f, 0);
       }
